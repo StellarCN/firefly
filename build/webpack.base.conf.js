dcc48d12973b479811b7d92f1e63480ce9762446
马兆永 <mazhaoyong@gmail.com>
2018-01-23 19:41:55 +0800

    webpack include ed25519-hk-key

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -40,7 +40,9 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules\/(?!(ed25519-hd-key)\/).*/,
+       // exclude: /node_modules\/(?!(ed25519-hd-key)\/).*/,
+        include: [ path.resolve(__dirname, "node_modules", "ed25519-hd-key")],
+        exclude: [ path.resolve(__dirname, "node_modules")],
       },
       {
         test: /\.json$/,
