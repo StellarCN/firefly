1cee4e1e47610e05a3817c8e51e89e992f013b0f
wangdepeng <iamdepeng@163.com>
2018-02-09 16:29:33 +0800

    myassets page

--- src/pages/asset/MyAssets.vue
+++ src/pages/asset/MyAssets.vue
@@ -16,20 +16,51 @@
       </v-btn>
       <span slot="switch_password">{{$t('Account.Password')}}</span>
     </toolbar>
+     <!--=======================================================-->
+    <div><!-- 试着改动的地方wdp-->
+           <div class="flex-row">
+                    <div class="flex2 textcenter">
+                        <div class="avatar-wrapper">
+                            <span class="avatar-back" @click="toNameCard"><i class="iconfont icon-erweima avatar"></i></span>
+                        </div>
+                    </div>
+                    <div class="flex4">
+                        <div class="title">{{account.name}}</div>
+                        <div class="address">{{account.address | shortaddress}}</div>
+                    </div>
+                </div>
+    </div>
+    <div id="TotalSum" class="test_TotalSum">
+      <span>总资产={{TotalSum}}</span><!-- 要改成资产数组数据的累加的和-->
+          <select class="test_select" >
+              <option selected value="watermelon">XCN</option>
+              <option  value="apple">XLM</option>
+              <option value="banana">BTC</option>
+          </select>
+    </div>
+    <div class="test_sort">
+        <!-- <input  type="button" value="隐藏资产" class="test_sort_hidden" v-on:click="hiddenMyAssets(item)"/>  -->
+        <input  type="button" :value="is_Flag === 'filter_zero'?/*$t('ShowZeroAsset')*/'显示0资产': '隐藏0资产'" class="test_sort_hidden" @click="hiddenMyAssets"/> 
+        <select class="test_sort_byValue" v-model="sort_flag">
+          <option value="none">默认排序</option>
+          <option value="name">按名称排序</option>
+          <option value="balance">按资产排序</option>
+        </select>
+    </div>
     <scroll :refresh="refresh">
-    <div class="content">
-      <card padding="0px 0px" class="">
+      <!--
+    <card padding="0px 0px" class="" v-for="item in prices" >
         <div class="assets" slot="card-content" v-if="balances && balances.length>0">
           <v-layout class="myassets-li" row wrap v-swiper=2 @click.stop="toAsset(native)">
             <v-flex xs4 class="myassets-wrapper">
               <div class="myassets">
-                <div class="myassets-name">{{native.code}}</div>
-                <div class="myassets-issuer">{{native.issuer}}</div>
+                <div class="myassets-name">{{item.code}}</div>
+                <div class="myassets-issuer">{{item.issuer}}</div>
               </div>
             </v-flex>
             <v-flex xs8 class="myassets-wrapper">
               <div class="myassets-balance">
-                 <span class="balance">{{native.balance}}</span>
+                 <span class="balance" v-show="is_Flag">{{item.price}}</span>
                  <span class="label">{{$t('Total')}}</span>
               </div>
               <div class="myassets-reserve">
@@ -38,16 +69,17 @@
               </div>
             </v-flex>
           </v-layout>
-          <div class="operate-box">
-            <div class="send" @click.stop="send(native)">{{$t('Send')}}</div>
-            <div class="receive" @click.stop="receive(native)">{{$t('Receive')}}</div>
-          </div>
         </div>
       </card>
+      -->
+        <!--=======================================================-->
+   
+    <div class="content">
+      
 
       <card padding="0px 0px" margin="20px 0px" class="infocard thirdassets">
         <div class="assets" slot="card-content">
-          <div class="assets-row" v-for="item in assets" :key="item.issuer||item.code">
+          <div class="assets-row" v-for="item in assets" :key="item.issuer+item.code">
             <v-layout class="myassets-li third-li" row wrap v-swiper=3 @click.stop="toAsset(item)">
             <v-flex xs4 class="myassets-wrapper">
               <div class="myassets">
@@ -72,14 +104,17 @@
         </div>
       </card>
     </div>
+  
     </scroll>
 
+  
   <tab-bar/>
   
    <bottom-notice :show.sync="notice" :text="noticeText">    </bottom-notice>
    <loading :show="working" :loading="working" :success="delok" :fail='delerror' />
    <password-sheet v-if="needpwd" @cancel="cancelpwd" @ok="checkpwd" />
   </div>
+
 </template>
 
 <script>
@@ -94,6 +129,17 @@ import backbutton from '@/mixins/backbutton'
 import loadaccount from '@/mixins/loadaccount'
 import Scroll from '@/components/Scroll'
 import TabBar from '@/components/TabBar'
+import _ from 'lodash'
+//import { Decimal } from 'decimal.js'
+
+//过滤0资产
+const FLAG_FILTER_ZERO = 'filter_zero'
+//不过滤资产
+const FLAG_DEFAULT = 'none'
+//按名称排序
+const SORT_NAME = 'name'
+const SORT_BANLANCE = 'balance'
+const SORT_DEFAULT = 'none'
 export default {
   data(){
     return {
@@ -108,6 +154,17 @@ export default {
       delerror: false,
 
       needpwd: false,
+      TotalSum:10,
+      is_Flag: FLAG_DEFAULT,
+      sort_flag: SORT_DEFAULT,
+      prices: [
+        {
+          code: 'XCN',
+          issuer: 'fchain.io',
+          price: 100,//XCN
+        }
+      ]
+      
 
     }
   },
@@ -134,7 +191,17 @@ export default {
           data.push(_.defaultsDeep({}, element))
         }
       })
-      return data
+       
+      //  let data = []
+      //  for(var i=0,n=this.balances.length; i<n;i++){
+      //    var obj = _.defaultsDeep({}, this.balances[i])
+      //    if(this.is_Flag === FLAG_FILTER_ZERO){
+      //     if(obj.balance === 0)continue; 
+      //    }
+      //   data.push(obj);
+      //  }
+      // console.log(data)
+      //  return data;
     },
   
   },
@@ -149,6 +216,13 @@ export default {
    })
   },
   methods: {
+    toNameCard(){
+
+    },
+    //隐藏资产---------------------------------------------------------------
+    hiddenMyAssets() {
+      this.is_Flag = this.is_Flag === FLAG_FILTER_ZERO ? FLAG_DEFAULT : FLAG_FILTER_ZERO
+    },
     ...mapActions({
       selectAsset:'selectAsset',
       delTrust:'delTrust',
@@ -232,10 +306,15 @@ export default {
     'loading': Loading,
   }
 }
+
 </script>
 
 
 <style lang="stylus" scoped>
+.hiddenPrice{
+  display :none
+}
 @require '~@/stylus/asset.styl'
+  @require '~@/stylus/settings.styl'
 </style>
 
